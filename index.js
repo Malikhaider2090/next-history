ae11eb775b7e8a9ff16762e11705661c00ca913e
Dev <188459604+Malikhaider2090@users.noreply.github.com>
2026-02-11 22:37:30 +0500

    setup

---
+++ index.js
@@ -0,0 +1,23 @@
+import jsonfile from "jsonfile";
+import moment from "moment";
+import simpleGit from "simple-git"
+import random from "random"
+
+const path = "./data.json";
+const makeCommits = (n) => {
+    if (n === 0) return simpleGit.push()
+    const x = Math.floor(Math.random() * 55);
+    const y = Math.floor(Math.random() * 7);
+
+    const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+
+    const data = {
+        date: date,
+    }
+    console.log(date)
+    jsonfile.writeFile(path, data, () => {
+        simpleGit().add([path]).commit(date, { "--date": date }, makeCommits.bind(this, --n));
+    })
+}
+
+makeCommits(100)
